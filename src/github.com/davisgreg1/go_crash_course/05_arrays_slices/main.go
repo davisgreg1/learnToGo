e38083fef3aa6ef5834d942a94540000c64fe7da
SC-GDAVIS-OSX <sc-gdavis-osx@SC-GDAVIS-OSX.local>
2018-12-19 13:27:43 -0600

    arrays and conditionals

--- src/github.com/davisgreg1/go_crash_course/05_arrays_slices/main.go
+++ src/github.com/davisgreg1/go_crash_course/05_arrays_slices/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hey")
+	// Arrays
+	// var fruitArr [2]string
+	// Assign Vals
+	// fruitArr[0] = "Apple"
+	// fruitArr[1] = "Banana"
+
+	// Declare and Assign shortHand
+	// fruitArr := [2]string{"Apple", "Banana"}
+
+	fruitSlice := []string{"Apple", "Orange", "Banana"}
+
+	fmt.Println(fruitSlice)
+	fmt.Println(len(fruitSlice))
+	fmt.Println(fruitSlice[1:2])
 }
